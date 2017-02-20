4c632ee3d461ef8e8709c2dd94507fbcce00ec45
Terralux <kenneth@berle.eu>
2017-02-20 15:42:37 +0100

    Added initial setup

---
+++ Pandora/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour {
+
+	public enum AnimationState{
+		LAND,
+		IDLE,
+		FLAP,
+		TAKEOFF,
+		FLY
+	}
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void UpdateAnimState(AnimationState newState){
+		if (anim == null) {
+			anim = GetComponent<Animator> ();
+		}
+		anim.SetTrigger (newState.ToString());
+	}
+}
