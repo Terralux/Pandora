29122afa2f271c0e5552e1a968f0d539c5d83998
Terralux <kenneth@berle.eu>
2017-03-02 17:33:24 +0100

    Merged with Istvans

---
+++ Pandora/Assets/Explosion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	int blendShapeCount;
+	SkinnedMeshRenderer skinnedMeshRenderer;
+	Mesh skinnedMesh;
+	float blendOne = 0f;
+	float blendTwo = 0f;
+	float blendSpeed = 1f;
+	bool blendOneFinished = false;
+
+	void Awake()
+	{
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+		skinnedMesh = GetComponent<SkinnedMeshRenderer> ().sharedMesh;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		blendShapeCount = skinnedMesh.blendShapeCount;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (blendShapeCount > 2) {
+
+			if (blendOne < 100f) {
+				skinnedMeshRenderer.SetBlendShapeWeight (0, blendOne);
+				blendOne += blendSpeed;
+			} else {
+				blendOneFinished = true;
+			}
+
+			if (blendOneFinished == true && blendTwo < 100f) {
+				skinnedMeshRenderer.SetBlendShapeWeight (1, blendTwo);
+				blendTwo += blendSpeed;
+			}
+
+		}
+		
+	}
+}
---
+++ Pandora/Assets/ParticlesManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlesManager : MonoBehaviour {
+
+	public ParticleSystem[] particleSystems;
+
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < particleSystems.Length; i++) {
+			particleSystems [i].Stop ();
+		}
+
+		StartCoroutine (NextParticleSetup ());
+	}
+
+	IEnumerator NextParticleSetup(){
+		yield return new WaitForSeconds (20);
+
+		int curIndex = Random.Range (0, particleSystems.Length);
+
+		for (int i = 0; i < particleSystems.Length; i++) {
+			if (curIndex == i) {
+				particleSystems [i].Play ();
+			} else {
+				particleSystems [i].Stop ();
+			}
+		}
+
+		StartCoroutine (NextParticleSetup ());
+	}
+}
