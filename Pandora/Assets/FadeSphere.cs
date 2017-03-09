c2b651dfa1665930825d0daa87d5b423e8a02de8
Terralux <kenneth@berle.eu>
2017-03-09 18:38:56 +0100

    Added more interaction to the scene

---
+++ Pandora/Assets/FadeSphere.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeSphere : BaseEvent {
+
+	public float delayBeforeActivation = 4f;
+	public float fadeSlowDownFactor = 1f;
+
+	private bool isFading = false;
+	private float currentFadeValue = 0f;
+
+	void Awake(){
+		Toolbox.RegisterEvent<FadeSphere> (this);
+	}
+
+	void Update(){
+		if (isFading) {
+			currentFadeValue -= Time.deltaTime/fadeSlowDownFactor;
+			GetComponent<MeshRenderer> ().material.color = new Color (0, 0, 0, currentFadeValue);
+			if (currentFadeValue <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	#region implemented abstract members of BaseEvent
+	public override void Action ()
+	{
+		StartCoroutine (WaitForDelay ());
+	}
+	#endregion
+
+	IEnumerator WaitForDelay(){
+		yield return new WaitForSeconds (delayBeforeActivation);
+		isFading = true;
+		currentFadeValue = 1f;
+	}
+	
+}
---
+++ Pandora/Assets/TriggerSphereFade.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSphereFade : MonoBehaviour {
+	public void Trigger(){
+		Toolbox.FindRequiredComponent<EventSystem> ().PlayNextEvent ();
+	}
+}
