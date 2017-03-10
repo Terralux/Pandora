b8235e196a976b5587433f1d2e7e39c1e4508401
Farki93 <cph-if36@cphbusiness.dk>
2017-03-11 00:08:46 +0100

    changed some stuff, and made build

---
+++ Pandora/Assets/Isti/Scripts/FlyinAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyinAnim : BaseEvent {
+
+	public string triggerName;
+	private Animator anim;
+	public float animDelay = 0f;
+
+
+
+	void Awake() {
+		Toolbox.RegisterEvent<FlyinAnim> (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	#region implemented abstract members of BaseEvent
+
+	public override void Action ()
+	{
+		anim.SetTrigger (triggerName);
+
+
+			StartCoroutine (WaitForFade ());
+
+	}
+
+	#endregion
+
+	IEnumerator WaitForFade(){
+		yield return new WaitForSeconds (animDelay);
+		Toolbox.FindRequiredComponent<EventSystem> ().PlayNextEvent ();
+	}
+}
